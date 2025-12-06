264d62598896225a96f70ec8c93f984d73ad2198
SAM's pc <Waliur Rafiq Sami>
2025-12-06 06:05:22 +0600

    feat(news-carousel): Implement responsive latest news section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroBanner from "@/components/home/HeroBanner";
+import LatestNewsSection from "./../components/home/LatestNewsSection";
 
 export default function Home() {
   return (
     <>
       <HeroBanner></HeroBanner>
+      <LatestNewsSection></LatestNewsSection>
     </>
   );
 }
