8b454bf09c3e3c50435c0c73af497d14a2cc32f2
SAM's pc <Waliur Rafiq Sami>
2025-12-08 16:37:35 +0600

    fix(LatestNewsSection-slider): Resolve latest news slider width issue on small and md screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Header></Header>
         {children}
         <Footer></Footer>
